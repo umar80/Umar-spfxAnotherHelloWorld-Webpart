2a9fa9d2558adb4349bb23460e16424ae5da898f
umar80 <umar80@users.noreply.github.com>
2018-08-12 09:50:57 +1000

    Initial commit

---
+++ src/webparts/umarAnotherHelloWorldTemplate/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IUmarAnotherHelloWorldTemplateWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'UmarAnotherHelloWorldTemplateWebPartStrings' {
+  const strings: IUmarAnotherHelloWorldTemplateWebPartStrings;
+  export = strings;
+}
